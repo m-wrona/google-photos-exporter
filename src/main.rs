4810e8654c99e2cee42e9c0ac580bfc39bd224fe
Michal Wronski <michal.robert.wronski@gmail.com>
2023-04-19 22:34:46 +0200

    project init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use crate::oauth::example;
+
+mod oauth;
+
+#[tokio::main]
+async fn main() {
+    example().await;
 }
---
+++ src/oauth.rs
@@ -0,0 +1,20 @@
+use hyper::body::HttpBody as _;
+use hyper::Client;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
+pub async fn example() {
+    println!("Hello, world!");
+    let client = Client::new();
+
+    // Parse an `http::Uri`...
+    let uri = "http://httpbin.org/ip".parse().unwrap();
+
+    // Await the response...
+    let mut resp = client.get(uri).await.unwrap();
+
+    println!("Response: {}", resp.status());
+
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk.unwrap()).await.unwrap();
+    }
+}
